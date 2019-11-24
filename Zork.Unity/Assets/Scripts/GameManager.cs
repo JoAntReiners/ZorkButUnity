469a5c23ffd84a31d4405ddcb4538fa20abf22e1
Joseph Winemberg <42650208+WinembergJo@users.noreply.github.com>
2019-11-23 21:27:39 -0500

    Video 1 Finished

---
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Zork;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private string ZorkGameFileAssetName = "Zork";
+
+    [SerializeField]
+    private UnityOutputService Output;
+
+    void Awake()
+    {
+        TextAsset gameJsonAsset = Resources.Load<TextAsset>(ZorkGameFileAssetName);
+        Game.Start(gameJsonAsset.text, Output);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using Zork;
+
+public class UnityOutputService : MonoBehaviour, IOutputService
+{
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Write(string value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Write(object value) => Write(value.ToString());
+
+    public void WriteLine(string value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void WriteLine(object value) => WriteLine(value.ToString());
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
